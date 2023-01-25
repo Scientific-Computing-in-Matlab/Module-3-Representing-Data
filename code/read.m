1fe10f1b3637be0a77fa16dfbeeb48d28a06db90
bartkrekelberg <bart@vision.rutgers.edu>
2023-01-25 08:22:27 -0500

    .

---
+++ code/read.m
@@ -0,0 +1,12 @@
+function [outputArg1,outputArg2] = read(inputArg1,inputArg2)
+% read - reads data from XXX
+%   Detailed explanation goes here
+
+%% First step
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+
+%% Second step
+
+end
---
+++ code/visualize.m
@@ -0,0 +1,16 @@
+function [outputArg1,outputArg2] = visualize(inputArg1,inputArg2)
+% visualize outcome of the analysis
+%   Detailed explanation goes here
+% INPUT
+% 
+% OUTPUT
+
+%% First figure
+% 
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+
+%% Second figure
+% 
+
+end
